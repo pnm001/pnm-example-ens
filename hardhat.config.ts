924c48848c448dffcb3b356be24ed424df3299a4
chronoskairos <pavel@pwnednomore.org>
2022-08-26 10:30:26 +0300

    Revert "Revert "[WebHook] #4""
    This reverts commit 99d785be61bb589f6300b386fd26f708e566c669.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,7 +25,6 @@ dotenv.config({ debug: false })
 
 task('accounts', 'Prints the list of accounts', async (_, hre) => {
   const accounts = await hre.ethers.getSigners()
-  
   for (const account of accounts) {
     console.log(account.address)
   }
